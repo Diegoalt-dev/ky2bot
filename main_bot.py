e452af25da4d7a4f9acb5987941deb7d66e68613
Diego Prieto <u1802753@unimilitar.edu.co>
2021-01-22 20:04:14 -0500

    ajuste a comando /actdb

--- main_bot.py
+++ main_bot.py
@@ -85,7 +85,6 @@ def lineas(update,context):
 
 def hi(update,context):
     num = random.randint(0,9)
-    kycursor = kydb.cursor()
     quer = "SELECT url FROM url_imag WHERE tipo = 'saludo' AND id = "+str(num+1)
     kycursor.execute(quer)
     resultado = kycursor.fetchall()
@@ -99,6 +98,7 @@ def actdb(update,context):
         password = "BVK9GOH4hnPr7PB9QcCp",
         database = 'bjngncktssejoh2aveqb'
         )
+        kycursor = kydb.cursor()
         update.message.reply_text(text = "Base de datos reconectada", quote = True)
     except:
         update.message.reply_text(text = "Error en reconexión a base de datos", quote = True)
